2ee63a0af04bb4b56c71b03b00fad9aa154c5ada
repetitioestmaterstudiorum <thotan@ik.me>
2022-08-14 18:30:57 +0200

    enforce_node_consistency method

--- w3-optimization/crossword/generate.py
+++ w3-optimization/crossword/generate.py
@@ -99,7 +99,10 @@ class CrosswordCreator():
         (Remove any values that are inconsistent with a variable's unary
          constraints; in this case, the length of the word.)
         """
-        raise NotImplementedError
+        for domain, words in self.domains.items():
+            for word in words.copy():
+                if len(word) is not domain.length:
+                    self.domains[domain].remove(word)
 
     def revise(self, x, y):
         """
