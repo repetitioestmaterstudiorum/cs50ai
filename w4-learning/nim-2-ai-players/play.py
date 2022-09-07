1b7c94bcca2e0efe1780b3894dc8fd1f8041b717
Thomas <44611591+repetitioestmaterstudiorum@users.noreply.github.com>
2022-09-08 00:52:21 +0200

    let 2 AIs play Nim against each other

---
+++ w4-learning/nim-2-ai-players/play.py
@@ -0,0 +1,55 @@
+from nim import train, play
+
+training_and_playing_rounds = 50
+
+training_rounds = 2000
+playing_rounds = 1000
+
+ai_alpha = 0.4
+ai_epsilon = 0.05
+
+ai2_alpha = 0.5
+ai2_epsilon = 0.1
+
+print(f"Training and playing rounds: {training_and_playing_rounds}")
+print(f"Training rounds per round: {training_rounds}")
+print(f"Playing rounds per round: {playing_rounds}\n")
+
+ai_won_rounds = 0
+ai2_won_rounds = 0
+
+for tapr in range(training_and_playing_rounds):
+    print(f"Overall round: {tapr + 1}/{training_and_playing_rounds}")
+
+    ai = train(training_rounds, 0.4, 0.1, 'ai')
+    ai2 = train(training_rounds, 0.3, 0.1, 'ai2')
+
+    ai_won = 0
+    ai2_won = 0
+
+    for round in range(playing_rounds):
+        winner = play(ai, ai2)
+        if winner == 'ai':
+            ai_won += 1
+        elif winner == 'ai2':
+            ai2_won += 1
+
+    round_winner = 'neihter!'
+    if ai_won > ai2_won:
+        ai_won_rounds += 1
+        round_winner = 'AI'
+    elif ai2_won > ai_won:
+        ai2_won_rounds += 1
+        round_winner = 'AI 2'
+
+    print(f"Training and playing round winner: {round_winner}\n")
+
+overall_winner = 'neither!'
+if ai_won_rounds > ai2_won_rounds:
+    overall_winner = 'AI'
+elif ai2_won_rounds > ai_won_rounds:
+    overall_winner = 'AI 2'
+
+print('WINNER: <<', overall_winner, '>>')
+print('AI   won:', f"{100 / training_and_playing_rounds * ai_won_rounds:.0f}% with settings: {ai_alpha} alpha, {ai_epsilon} epsilon ({ai_won_rounds}/{training_and_playing_rounds})")
+print('AI 2 won:', f"{100 / training_and_playing_rounds * ai2_won_rounds:.0f}% with settings: {ai2_alpha} alpha, {ai2_epsilon} epsilon ({ai2_won_rounds}/{training_and_playing_rounds})")
