e3a30a898f2290642516474318b92a3d5d004cec
repetitioestmaterstudiorum <44611591+repetitioestmaterstudiorum@users.noreply.github.com>
2022-09-10 23:24:05 +0200

    adjust nn example names

--- w5-neural-networks/nn-play/banknotes.py
+++ w5-neural-networks/nn-play/banknotes.py
@@ -26,7 +26,8 @@ X_training, X_testing, y_training, y_testing = train_test_split(
 model = tf.keras.models.Sequential()
 
 # Add a hidden layer with 8 units, with ReLU activation
-model.add(tf.keras.layers.Dense(8, input_shape=(4,), activation="relu"))
+model.add(tf.keras.layers.Dense(10, input_shape=(4,), activation="relu")) # 100% accurate after ~120 epochs
+# model.add(tf.keras.layers.Dense(10, input_shape=(4,), activation="tanh")) # 100% accurate after ~200 epochs
 
 # Add output layer with 1 unit, with sigmoid activation
 model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
@@ -37,7 +38,7 @@ model.compile(
     loss="binary_crossentropy",
     metrics=["accuracy"]
 )
-model.fit(X_training, y_training, epochs=20)
+model.fit(X_training, y_training, epochs=1000)
 
 # Evaluate how well model performs
 model.evaluate(X_testing, y_testing, verbose=2)
