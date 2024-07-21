0f8863117b40637045eb4f2382f4654e5fc4d50d
repetitioestmaterstudiorum <44611591+repetitioestmaterstudiorum@users.noreply.github.com>
2024-07-21 14:59:29 +0200

    use sys arg

--- w4-learning/nim/play.py
+++ w4-learning/nim/play.py
@@ -1,4 +1,10 @@
+import sys
 from nim import train, play
 
-ai = train(5000)
+DEFAULT_ROUNDS = 500
+
+rounds = int(sys.argv[1]) if len(sys.argv) > 1 else DEFAULT_ROUNDS
+
+ai = train(rounds)
+
 play(ai)
